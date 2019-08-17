d5726f59b3e954f999f8bdcda666099d481001b1
Brandon Chinn <brandon@leapyear.io>
2019-08-16 22:53:56 -0700

    Add basic header

---
+++ components/layout/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>WorshipMate</h1>
+      <ul>
+        <li>About</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+}
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import { default as NextApp } from "next/app";
+
+import Header from "components/layout/Header";
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Fragment>
+        <Header />
+        <Component {...pageProps} />
+      </Fragment>
+    );
+  }
+}
