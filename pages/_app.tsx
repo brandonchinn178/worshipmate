6554bbf9ce83e2f1088a25a0a790a80a10fa72b4
Brandon Chinn <brandonchinn178@gmail.com>
2019-08-17 22:31:24 -0700

    Rerun prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import React, { Fragment } from "react";
-import { default as NextApp } from "next/app";
+import React, { Fragment } from "react"
+import { default as NextApp } from "next/app"
 
-import Header from "components/layout/Header";
+import Header from "components/layout/Header"
 
 export default class App extends NextApp {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps } = this.props
     return (
       <Fragment>
         <Header />
         <Component {...pageProps} />
       </Fragment>
-    );
+    )
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return <div>Welcome to Next.js!</div>
 }
