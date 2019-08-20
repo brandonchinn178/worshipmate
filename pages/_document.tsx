edbfaf7d7196ec7c4b917404e13c33d3586f2863
Brandon Chinn <brandon@leapyear.io>
2019-08-19 22:49:39 -0700

    Add styled-components
    https://www.styled-components.com/docs/advanced#nextjs

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import {
+  default as NextDocument,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document"
+import { ServerStyleSheet } from "styled-components"
+
+export default class Document extends NextDocument {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await NextDocument.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
