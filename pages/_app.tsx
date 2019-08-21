b9a1464e3308f50a809d36ca6f8bdc9252cd8831
Brandon Chinn <brandon@leapyear.io>
2019-08-21 00:55:35 -0700

    Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import React, { Fragment, ReactElement } from 'react'
 import { default as NextApp } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
 import Header from '@components/layout/Header'
+import theme from '@components/theme'
 
 export default class App extends NextApp {
   render(): ReactElement {
     const { Component, pageProps } = this.props
     return (
-      <Fragment>
-        <Header />
-        <Component {...pageProps} />
-      </Fragment>
+      <ThemeProvider theme={theme}>
+        <Fragment>
+          <Header />
+          <Component {...pageProps} />
+        </Fragment>
+      </ThemeProvider>
     )
   }
 }
