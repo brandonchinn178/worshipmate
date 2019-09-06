835ca9cb3e618acf1348262d53811b480bda9d6e
Brandon Chinn <brandon@leapyear.io>
2019-09-05 20:12:40 -0700

    Split Home into grid

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,33 @@
 import { FC } from 'react'
+import styled from 'styled-components'
 
-const Home: FC = () => {
-  return <div>Welcome to Next.js!</div>
-}
+const Home: FC = () => (
+  <Grid>
+    <Sidebar>TODO: Sidebar</Sidebar>
+    <SearchBar>TODO: Search</SearchBar>
+    <SongTable>TODO: SongTable</SongTable>
+  </Grid>
+)
 
 export default Home
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 30% auto;
+  grid-template-rows: 50px auto;
+  grid-template-areas:
+    'sidebar search'
+    'sidebar table';
+`
+
+const Sidebar = styled.div`
+  grid-area: sidebar;
+`
+
+const SearchBar = styled.div`
+  grid-area: search;
+`
+
+const SongTable = styled.div`
+  grid-area: table;
+`
