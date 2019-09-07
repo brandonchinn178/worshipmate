1c737b5c55a1304b3a67c1d5fd3b590b1c508f58
Brandon Chinn <brandon@leapyear.io>
2019-09-06 23:29:27 -0700

    Add DataTable

--- client/lib/song/SongTable.tsx
+++ client/lib/song/SongTable.tsx
@@ -1,22 +1,31 @@
 import { FC } from 'react'
 
+import DataTable, { ColumnDefs } from '~/toolkit/DataTable'
+
 import { Song } from '.'
 
 type SongTableProps = {
   songs: Song[]
 }
 
+const songTableColumnDefs: ColumnDefs<Song> = [
+  {
+    name: 'title',
+    size: '2fr',
+  },
+  {
+    name: 'artist',
+    size: '2fr',
+  },
+  {
+    name: 'themes',
+    size: '3fr',
+    render: (song) => song.themes.join(', '),
+  },
+]
+
 const SongTable: FC<SongTableProps> = ({ songs }) => (
-  <div>
-    {songs.map(({ slug, title, artist, themes }) => (
-      <div key={slug} style={{ borderBottom: '1px solid black' }}>
-        <div>Slug: {slug}</div>
-        <div>Title: {title}</div>
-        <div>Artist: {artist}</div>
-        <div>Themes: {themes.join(', ')}</div>
-      </div>
-    ))}
-  </div>
+  <DataTable data={songs} columnDefs={songTableColumnDefs} />
 )
 
 export default SongTable
