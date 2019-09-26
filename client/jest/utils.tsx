7fc04fd7a37ce05dde041f97d4fc65a0dfee3647
Brandon Chinn <brandon@leapyear.io>
2019-09-25 18:43:18 -0700

    Add typechecking

--- client/jest/utils.tsx
+++ client/jest/utils.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react'
+import { ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import theme from '~/theme'
 import GlobalStyle from '~/theme/global'
 
-export const renderUI = (ui, options) =>
+export const renderUI = (ui: ReactElement, options = {}) =>
   render(ui, {
     wrapper: ({ children }) => (
       <ThemeProvider theme={theme}>
