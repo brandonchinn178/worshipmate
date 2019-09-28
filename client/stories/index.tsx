54e2d667f37c4777071a231df14a4e81e985e761
Brandon Chinn <brandonchinn178@gmail.com>
2019-09-27 23:15:43 -0700

    Add testing and CI (#9)
    * Add return type for Story
    
    * Add jest packages
    
    * Add tests
    
    * Add typechecking
    
    * Move storybook package.json command
    
    * Remove storybook babel options
    
    * Move client/lib/stories to client/stories
    
    * Fix linting/typechecking
    
    * Add CI config

--- client/stories/index.tsx
+++ client/stories/index.tsx
@@ -1,11 +1,11 @@
-import { storiesOf } from '@storybook/react'
+import { storiesOf, Story as StoryInterface } from '@storybook/react'
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components'
 
 import theme from '~/theme'
 import GlobalStyle from '~/theme/global'
 
-export default function Story(name: string): unknown {
+export default function Story(name: string): StoryInterface {
   return storiesOf(name, module).addDecorator((storyFn) => (
     <ThemeProvider theme={theme}>
       <Layout>
