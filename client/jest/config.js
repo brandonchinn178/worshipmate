95bfcdb4940c4bf2ab4adb96c96bece11b8f7796
Brandon Chinn <brandonchinn178@gmail.com>
2019-09-30 22:12:03 -0700

    Lint (#10)
    * Run prettier in eslint
    
    * Fix linting
    
    * Add --no-ignore to pre-commit config
    
    * Pin typescript to 3.5.3 for eslint

--- client/jest/config.js
+++ client/jest/config.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-const roots = process.env.STORYSHOTS_DIR ? ['<rootDir>/stories/'] : ['<rootDir>/lib', '<rootDir>/pages']
+const roots = process.env.STORYSHOTS_DIR
+  ? ['<rootDir>/stories/']
+  : ['<rootDir>/lib', '<rootDir>/pages']
 
 module.exports = {
   rootDir: path.join(__dirname, '..'),
