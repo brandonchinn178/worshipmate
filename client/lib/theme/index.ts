3df020d6f46f24e454a8b2d1378a7e07096877d9
Brandon Chinn <brandon@leapyear.io>
2019-11-06 23:26:47 -0800

    Add styled functions

--- client/lib/theme/index.ts
+++ client/lib/theme/index.ts
@@ -1,3 +1,5 @@
+import { InterpolationFunction, ThemeProps } from 'styled-components'
+
 import { colors } from './colors'
 import { fontFamilies, fonts } from './fonts'
 
@@ -6,3 +8,39 @@ export const theme = {
   fontFamilies,
   fonts,
 }
+
+type ThemeInterface = typeof theme
+
+/**
+ * A styled-components function for using a theme value.
+ */
+type StyledFunction<T> = (
+  name: keyof T,
+) => InterpolationFunction<ThemeProps<ThemeInterface>>
+
+/**
+ * Usage:
+ *   styled.div`
+ *     background: ${color('darkTeal')}
+ *   `
+ */
+export const color: StyledFunction<typeof colors> = (name) => (p) =>
+  p.theme.colors[name]
+
+/**
+ * Usage:
+ *   styled.div`
+ *     font-family: ${fontFamily('alegreya')}
+ *   `
+ */
+export const fontFamily: StyledFunction<typeof fontFamilies> = (name) => (p) =>
+  p.theme.fontFamilies[name]
+
+/**
+ * Usage:
+ *   styled.div`
+ *     ${font('label')}
+ *   `
+ */
+export const font: StyledFunction<typeof fonts> = (name) => (p) =>
+  p.theme.fonts[name]
