97573c4ff7d6d0a322617433db7d590693a7a8b9
Brandon Chinn <brandon@leapyear.io>
2019-11-23 21:39:36 -0800

    Initial sidebar commit

---
+++ client/lib/song/SongFilter.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+import { color } from '~/theme'
+
+export default function SongFilter() {
+  return <FilterBox>TODO</FilterBox>
+}
+
+const FilterBox = styled.div`
+  padding: 20px;
+
+  border: 1px solid ${color('black')};
+  box-shadow: 0 0 4px ${color('lightGray')};
+`
