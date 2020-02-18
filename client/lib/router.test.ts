c2164f89bb5ce297e2b6b2794d65396c90675cb3
Brandon Chinn <brandon@leapyear.io>
2020-02-17 16:19:20 -0800

    Add generic modifyQueryString helper

--- client/lib/router.test.ts
+++ client/lib/router.test.ts
@@ -1,20 +1,20 @@
 import * as _ from 'lodash'
 import Router, { NextRouter } from 'next/router'
 
-import { setQueryString } from './router'
+import { modifyQueryString, setQueryString } from './router'
+
+const mkRouter = ({ query }: { query: NextRouter['query'] }) => {
+  const router = _.clone(Router)
+  router.query = query
+  router.push = jest.fn()
+  return router
+}
+
+const getNewQuery = (router: NextRouter): object => {
+  return _.last((router.push as jest.Mock).mock.calls)[0].query
+}
 
 describe('setQueryString', () => {
-  const mkRouter = ({ query }: { query: NextRouter['query'] }) => {
-    const router = _.clone(Router)
-    router.query = query
-    router.push = jest.fn()
-    return router
-  }
-
-  const getNewQuery = (router: NextRouter): object => {
-    return _.last((router.push as jest.Mock).mock.calls)[0].query
-  }
-
   it('sets a querystring value', () => {
     const router = mkRouter({
       query: {
@@ -50,6 +50,46 @@ describe('setQueryString', () => {
     })
 
     setQueryString(router, 'foo', undefined)
+    expect(getNewQuery(router)).toMatchObject({})
+  })
+})
+
+describe('modifyQueryString', () => {
+  it('modifies a querystring value', () => {
+    const router = mkRouter({
+      query: {
+        foo: 'hello',
+      },
+    })
+
+    modifyQueryString(router, 'foo', (v) => (v ? v + '_world' : 'new'))
+    expect(getNewQuery(router)).toMatchObject({
+      foo: 'hello_world',
+    })
+  })
+
+  it('sets a new querystring value', () => {
+    const router = mkRouter({
+      query: {
+        foo: 'bar',
+      },
+    })
+
+    modifyQueryString(router, 'foo2', (v) => (v ? v + '_world' : 'new'))
+    expect(getNewQuery(router)).toMatchObject({
+      foo: 'bar',
+      foo2: 'new',
+    })
+  })
+
+  it('unsets a querystring value', () => {
+    const router = mkRouter({
+      query: {
+        foo: 'bar',
+      },
+    })
+
+    modifyQueryString(router, 'foo', () => null)
     expect(getNewQuery(router)).toMatchObject({})
   })
 })
