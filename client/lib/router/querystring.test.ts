509bffad620d1eb9f636dcb9e04f7b39a7222111
Brandon Chinn <brandon@leapyear.io>
2020-02-17 17:32:53 -0800

    Make lib/router.ts a directory

--- client/lib/router/querystring.test.ts
+++ client/lib/router/querystring.test.ts
@@ -1,18 +1,5 @@
-import * as _ from 'lodash'
-import Router, { NextRouter } from 'next/router'
-
-import { modifyQueryString, setQueryString } from './router'
-
-const mkRouter = ({ query }: { query: NextRouter['query'] }) => {
-  const router = _.clone(Router)
-  router.query = query
-  router.push = jest.fn()
-  return router
-}
-
-const getNewQuery = (router: NextRouter): object => {
-  return _.last((router.push as jest.Mock).mock.calls)[0].query
-}
+import { modifyQueryString, setQueryString } from './querystring'
+import { getNewQuery, mkRouter } from './testutils'
 
 describe('setQueryString', () => {
   it('sets a querystring value', () => {
