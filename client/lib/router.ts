0a2a3fff09a0efb6bf51c2a3661ee8d98198225c
Brandon Chinn <brandonchinn178@gmail.com>
2020-02-17 16:19:20 -0800

    Add generic modifyQueryString helper

--- client/lib/router.ts
+++ client/lib/router.ts
@@ -1,10 +1,15 @@
 import { NextRouter } from 'next/router'
 
-export const setQueryString = (
+// https://github.com/zeit/next.js/blob/3f691eaa45e8f802f9977ff050f66ac64f016e74/packages/next/next-server/lib/router/router.ts#L37
+// https://github.com/sindresorhus/query-string/blob/7712622f5ae8cc1f99ae45f4af1a1965efec44ac/index.d.ts#L127
+type QueryStringValue = string | string[] | null | undefined
+
+export const modifyQueryString = (
   router: NextRouter,
   key: string,
-  value?: string,
+  callback: (value: QueryStringValue) => QueryStringValue,
 ) => {
+  const value = callback(router.query[key])
   const query = {
     ...router.query,
     [key]: value,
@@ -18,4 +23,12 @@ export const setQueryString = (
     pathname: router.pathname,
     query,
   })
+}
+
+export const setQueryString = (
+  router: NextRouter,
+  key: string,
+  value?: string,
+) => {
+  modifyQueryString(router, key, () => value)
 }
