312878b88873a52365b533d07c1ed5af5adb4d1b
Brandon Chinn <brandonchinn178@gmail.com>
2020-02-17 17:32:53 -0800

    Make lib/router.ts a directory

--- client/lib/router/querystring.ts
+++ client/lib/router/querystring.ts
@@ -2,7 +2,7 @@ import { NextRouter } from 'next/router'
 
 // https://github.com/zeit/next.js/blob/3f691eaa45e8f802f9977ff050f66ac64f016e74/packages/next/next-server/lib/router/router.ts#L37
 // https://github.com/sindresorhus/query-string/blob/7712622f5ae8cc1f99ae45f4af1a1965efec44ac/index.d.ts#L127
-type QueryStringValue = string | string[] | null | undefined
+export type QueryStringValue = string | string[] | null | undefined
 
 export const modifyQueryString = (
   router: NextRouter,
---
+++ client/lib/router/testutils.ts
@@ -0,0 +1,13 @@
+import * as _ from 'lodash'
+import Router, { NextRouter } from 'next/router'
+
+export const mkRouter = ({ query }: { query: NextRouter['query'] }) => {
+  const router = _.clone(Router)
+  router.query = query
+  router.push = jest.fn()
+  return router
+}
+
+export const getNewQuery = (router: NextRouter): object => {
+  return _.last((router.push as jest.Mock).mock.calls)[0].query
+}
