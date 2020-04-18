43828a6524e97c4137b86f48a8be56790a0252bb
Brandon Chinn <brandonchinn178@gmail.com>
2020-04-18 15:20:44 -0700

    Add 'songs' query resolver

--- server/src/song/song.module.ts
+++ server/src/song/song.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { Song } from './song.entity'
 import { SongResolver } from './song.resolver'
+import { SongService } from './song.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Song])],
-  providers: [SongResolver],
+  providers: [SongResolver, SongService],
 })
 export class SongModule {}
