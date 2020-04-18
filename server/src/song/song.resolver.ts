97beca78e26c00e2e2bf071b3cd8df47e8c41ce6
Brandon Chinn <brandon@leapyear.io>
2020-04-18 15:20:44 -0700

    Add 'songs' query resolver

--- server/src/song/song.resolver.ts
+++ server/src/song/song.resolver.ts
@@ -1,9 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 
-@Resolver()
+import { Song } from './song.entity'
+import { SongService } from './song.service'
+
+@Resolver(() => Song)
 export class SongResolver {
-  @Query((_returns) => String)
-  test() {
-    return 'Test!'
+  constructor(private songService: SongService) {}
+
+  @Query((_returns) => [Song], { name: 'songs' })
+  async searchSongs(@Args('query') query: string) {
+    return this.songService.searchSongs(query)
   }
 }
