8c04abe0bc92c3c0c745e4fae9e261280ab34869
Brandon Chinn <brandonchinn178@gmail.com>
2020-04-18 18:58:15 -0700

    Separate hooks for setting up a db connection

--- server/__test__/setup.ts
+++ server/__test__/setup.ts
@@ -5,22 +5,44 @@ import {
   getConnectionOptions,
 } from 'typeorm'
 
+// A helper for running an action with a database connection.
+const withConnection = async (
+  name: string,
+  callback: (conn: Connection) => Promise<void>,
+) => {
+  let conn, isOriginallyConnected
+  try {
+    conn = await createConnection(name)
+    isOriginallyConnected = false
+  } catch (_) {
+    conn = await getConnection(name)
+    isOriginallyConnected = true
+  }
+
+  try {
+    await callback(conn)
+  } finally {
+    if (!isOriginallyConnected) {
+      await conn.close()
+    }
+  }
+}
+
 // A helper to manage the test database itself.
 const manageTestDatabase = async (
   callback: (conn: Connection, testDatabase: string) => Promise<void>,
 ): Promise<void> => {
-  const originalConnection = await createConnection('original')
   const testDatabase = (await getConnectionOptions()).database as string
 
-  try {
+  await withConnection('original', async (originalConnection) => {
     await callback(originalConnection, testDatabase)
-  } finally {
-    await originalConnection.close()
-  }
+  })
 }
 
 // Set up Jest setup/teardown hooks to set up a test database.
-const setupConnection = () => {
+const setupTestDatabase = () => {
+  let beforeAllSucceeded = false
+
   beforeAll(async () => {
     // create test database
     await manageTestDatabase(async (conn, testDatabase) => {
@@ -31,33 +53,35 @@ const setupConnection = () => {
           `WARNING: test database '${testDatabase}' already exists. Please drop it.`,
         )
       }
+
+      beforeAllSucceeded = true
     })
   })
 
   beforeEach(async () => {
-    const connection = await createConnection()
+    await withConnection('default', async (connection) => {
+      // clear database
+      await connection.transaction(async (manager) => {
+        const tables = await manager.query(`
+          SELECT table_name AS table FROM information_schema.tables
+          WHERE table_schema = 'public' AND table_type = 'BASE TABLE'
+        `)
 
-    // clear database
-    await connection.transaction(async (manager) => {
-      const tables = await manager.query(`
-        SELECT table_name AS table FROM information_schema.tables
-        WHERE table_schema = 'public' AND table_type = 'BASE TABLE'
-      `)
+        const truncationList = tables.map(
+          ({ table }: { table: string }) => `public.${table}`,
+        )
 
-      const truncationList = tables.map(
-        ({ table }: { table: string }) => `public.${table}`,
-      )
-
-      await manager.query(
-        `TRUNCATE ${truncationList.join(',')} RESTART IDENTITY`,
-      )
+        await manager.query(
+          `TRUNCATE ${truncationList.join(',')} RESTART IDENTITY`,
+        )
+      })
     })
   })
 
   afterAll(async () => {
-    // close test database connection
-    const connection = await getConnection()
-    await connection.close()
+    if (!beforeAllSucceeded) {
+      return
+    }
 
     // drop test database
     await manageTestDatabase(async (conn, testDatabase) => {
@@ -76,5 +100,5 @@ switch (TEST_TYPE) {
   case 'all':
   case 'integration':
   case 'e2e':
-    setupConnection()
+    setupTestDatabase()
 }
