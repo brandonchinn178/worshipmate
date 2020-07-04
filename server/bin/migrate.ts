896ee8bc597389731f04dc1558985a17aa0d25a7
Brandon Chinn <brandonchinn178@gmail.com>
2020-07-04 15:39:59 -0700

    Add song migrations

---
+++ server/bin/migrate.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env ts-node
+
+import { initDatabase } from '~/db'
+
+const runMigrations = async () => {
+  const db = initDatabase()
+  const options = {
+    checkOrder: true,
+    loadFromArgs: true,
+  }
+
+  try {
+    await db.migrate(options)
+    console.log('Migrations complete')
+  } finally {
+    db.close()
+  }
+}
+
+runMigrations()
