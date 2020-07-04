74886783fb4a567ec87b1f7a84def77c5974d02d
Brandon Chinn <brandonchinn178@gmail.com>
2020-07-04 16:40:03 -0700

    Add Song API e2e tests

---
+++ server/__test__/utils/db.ts
@@ -0,0 +1,37 @@
+import { Database, sql } from 'pg-toolbox'
+
+require('dotenv').config()
+const TEST_DB = 'worship_mate_test'
+
+export const setupTestDatabase = (): Database => {
+  beforeAll(async () => {
+    const admin = new Database({ database: 'postgres' })
+
+    try {
+      await admin.query(sql`DROP DATABASE IF EXISTS ${sql.quote(TEST_DB)}`)
+      await admin.query(sql`CREATE DATABASE ${sql.quote(TEST_DB)}`)
+    } finally {
+      await admin.close()
+    }
+  })
+
+  const db = new Database({ database: TEST_DB })
+
+  beforeAll(async () => {
+    await db.migrate({
+      log: () => {
+        // suppress migration output
+      },
+    })
+  })
+
+  beforeEach(async () => {
+    await db.clear()
+  })
+
+  afterAll(async () => {
+    await db.close()
+  })
+
+  return db
+}
---
+++ server/src/song/api.e2e-spec.ts
@@ -0,0 +1,88 @@
+import { setupTestDatabase } from '~test-utils/db'
+
+import { SongAPI } from './api'
+
+describe('SongAPI', () => {
+  const db = setupTestDatabase()
+  const songApi = new SongAPI(db)
+
+  describe('searchSongs', () => {
+    const allSongs = [
+      {
+        slug: 'blessed-be-your-name',
+        title: 'Blessed Be Your Name',
+        recommended_key: 'A',
+        time_signature_top: 4,
+        time_signature_bottom: 4,
+        bpm: 140,
+      },
+      {
+        slug: 'build-my-life',
+        title: 'Build My Life',
+        recommended_key: 'E',
+        time_signature_top: 4,
+        time_signature_bottom: 4,
+        bpm: 68,
+      },
+      {
+        slug: 'ever-be',
+        title: 'Ever Be',
+        recommended_key: 'E',
+        time_signature_top: 4,
+        time_signature_bottom: 4,
+        bpm: 72,
+      },
+    ]
+
+    beforeEach(async () => {
+      await db.insertAll('song', allSongs)
+    })
+
+    it('can return all songs', async () => {
+      const songs = await songApi.searchSongs()
+      expect(songs).toMatchObject([
+        {
+          id: expect.any(Number),
+          slug: 'blessed-be-your-name',
+          title: 'Blessed Be Your Name',
+          recommendedKey: 'A',
+          timeSignature: {
+            top: 4,
+            bottom: 4,
+          },
+          bpm: 140,
+        },
+        {
+          id: expect.any(Number),
+          slug: 'build-my-life',
+          title: 'Build My Life',
+          recommendedKey: 'E',
+          timeSignature: {
+            top: 4,
+            bottom: 4,
+          },
+          bpm: 68,
+        },
+        {
+          id: expect.any(Number),
+          slug: 'ever-be',
+          title: 'Ever Be',
+          recommendedKey: 'E',
+          timeSignature: {
+            top: 4,
+            bottom: 4,
+          },
+          bpm: 72,
+        },
+      ])
+    })
+
+    it('can return songs matching a query', async () => {
+      const songs = await songApi.searchSongs('be')
+      expect(songs).toMatchObject([
+        { title: 'Blessed Be Your Name' },
+        { title: 'Ever Be' },
+      ])
+    })
+  })
+})
