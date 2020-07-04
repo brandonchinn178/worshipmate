63b593e181c44d27ec0fc12233a45cc419cb2c7a
Brandon Chinn <brandon@leapyear.io>
2020-07-04 15:39:57 -0700

    Add database connection

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { initServer } from './apollo'
+import { initDatabase } from './db'
 
-const server = initServer()
+const db = initDatabase()
+const server = initServer(db)
+
 server.listen(4000).then(({ url }: { url: string }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
--- server/src/song/api.ts
+++ server/src/song/api.ts
@@ -1,14 +1,34 @@
 import { DataSource } from 'apollo-datasource'
+import { Database, sql } from 'pg-toolbox'
 
 import { Song } from './models'
+import { SongRecord } from './schema'
+
+const fromRecord = (song: SongRecord): Song => ({
+  ...song,
+  recommendedKey: song.recommended_key,
+  timeSignature: {
+    top: song.time_signature_top,
+    bottom: song.time_signature_bottom,
+  },
+})
 
 export class SongAPI extends DataSource {
-  constructor() {
+  constructor(private readonly db: Database) {
     super()
   }
 
   async searchSongs(query?: string): Promise<Song[]> {
-    console.log(query)
-    return []
+    const filters = []
+
+    if (query) {
+      filters.push(sql`"song"."title" ILIKE ${'%' + query + '%'}`)
+    }
+
+    const songs = await this.db.query<SongRecord>(sql`
+      SELECT * FROM "song" WHERE ${sql.and(filters)}
+    `)
+
+    return songs.map(fromRecord)
   }
 }
