b5aa4965994188e4757fae637242e3b9302f41eb
Brandon Chinn <brandonchinn178@gmail.com>
2020-11-15 09:10:09 -0800

    Convert custom app into function component

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { default as NextApp } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components'
@@ -7,24 +7,21 @@ import Header from '~/layout/Header'
 import { theme } from '~/theme'
 import GlobalStyle from '~/theme/global'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-        <>
-          <Head>
-            <title>WorshipMate</title>
-          </Head>
-          <GlobalStyle />
-          <Header />
-          <PageContent>
-            <Component {...pageProps} />
-          </PageContent>
-        </>
-      </ThemeProvider>
-    )
-  }
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <Head>
+          <title>WorshipMate</title>
+        </Head>
+        <GlobalStyle />
+        <Header />
+        <PageContent>
+          <Component {...pageProps} />
+        </PageContent>
+      </>
+    </ThemeProvider>
+  )
 }
 
 const PageContent = styled.div`
