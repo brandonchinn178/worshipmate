7e874ce1283a8481b31432ed405b3bf547522e98
Brandon Chinn <brandonchinn178@gmail.com>
2020-11-23 15:40:06 -0800

    Move webpack config into main.js

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,3 +1,27 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../lib/**/*.stories.tsx'],
+  webpackFinal: ({ config }) => {
+    config.module.rules = [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ]
+
+    config.resolve.alias['~'] = path.join(__dirname, '..', 'lib')
+    config.resolve.alias['~stories'] = path.join(__dirname, '..', 'stories')
+    config.resolve.extensions.push('.ts', '.tsx')
+
+    return config
+  },
 }
