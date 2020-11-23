21b54d882cf2755ede80e722ebc371607a26e74b
Brandon Chinn <brandonchinn178@gmail.com>
2020-11-23 15:38:39 -0800

    Move decorator into preview.tsx

---
+++ client/.storybook/preview.tsx
@@ -0,0 +1,19 @@
+import styled, { ThemeProvider } from 'styled-components'
+
+import { theme } from '~/theme'
+import GlobalStyle from '~/theme/global'
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <GlobalStyle />
+        <Story />
+      </Layout>
+    </ThemeProvider>
+  ),
+]
+
+const Layout = styled.div`
+  padding: 20px;
+`
