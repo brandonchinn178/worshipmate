5c80dc216b96235356a3b9746f830da5577153bc
Brandon Chinn <brandonchinn178@gmail.com>
2020-11-23 16:03:58 -0800

    Configure controls

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   stories: ['../lib/**/*.stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.module.rules = [
       {
