ef36a075597f7181809fead52b8884920824f340
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-05 23:39:41 -0800

    Fix searchFilter tests

--- server/src/song/searchFilter.ts
+++ server/src/song/searchFilter.ts
@@ -34,7 +34,7 @@ const FILTER_VALUE_SCHEMA: Record<string, yup.Schema<unknown>> = {
   RECOMMENDED_KEY: yup.string().required(),
   BPM: positiveIntSchema.required(),
   TIME_SIGNATURE: pairSchema.of(positiveIntSchema.required()).required(),
-  THEMES: yup.array().of(yup.string().required()).required(),
+  THEMES: yup.array().of(yup.string().required()).required().min(1),
 }
 
 /**
