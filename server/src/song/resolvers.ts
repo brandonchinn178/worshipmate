ad6d3a5414bd8bc2e18689aeb9b20b3e277fbf74
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-23 11:11:09 -0800

    Remove SongSearchResult from resolvers

--- server/src/song/resolvers.ts
+++ server/src/song/resolvers.ts
@@ -1,56 +1,26 @@
 import * as _ from 'lodash'
 
 import { QueryParent, Resolver, Resolvers } from '~/graphql/resolvers'
-import { AvailableFilter, QuerySearchSongsArgs, Song } from '~/graphql/types'
+import { QuerySearchSongsArgs, Song } from '~/graphql/types'
 
-import { SearchOptions } from './api'
 import { validateSearchFilter } from './searchFilter'
-
-type SongSearchResultParent = SearchOptions
 
 type QueryResolvers = Resolvers<
   QueryParent,
   {
-    searchSongs: Resolver<QuerySearchSongsArgs, SongSearchResultParent>
+    searchSongs: Resolver<QuerySearchSongsArgs, Song[]>
   }
 >
 
 const Query: QueryResolvers = {
-  searchSongs(parent, args) {
+  searchSongs(parent, args, { dataSources: { songAPI } }) {
     const { query, filters } = args
 
-    const searchOptions = {
+    return songAPI.searchSongs({
       query: query ?? undefined,
       filters: _.map(filters, validateSearchFilter),
-    }
-    return searchOptions
-  },
-}
-
-type SongSearchResultResolvers = Resolvers<
-  SongSearchResultParent,
-  {
-    songs: Resolver<Song[]>
-    availableFilters: Resolver<AvailableFilter[]>
-  }
->
-
-const SongSearchResult: SongSearchResultResolvers = {
-  async songs(parent, args, { dataSources: { songAPI } }) {
-    const searchOptions = parent
-
-    const songs = await songAPI.searchSongs(searchOptions)
-    return songs
-  },
-
-  async availableFilters(parent, args, { dataSources: { songAPI } }) {
-    const searchOptions = parent
-
-    const availableFilters = await songAPI.getAvailableFilters(searchOptions)
-    return _.map(availableFilters, (values, name) => {
-      return { name, values } as AvailableFilter
     })
   },
 }
 
-export const resolvers = { Query, SongSearchResult }
+export const resolvers = { Query }
