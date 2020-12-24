f6ea3a3ddca1a0541fcd26432e4b8337a446e7a3
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-23 16:28:52 -0800

    Test mkFilterHandler

--- client/lib/router/testutils.ts
+++ client/lib/router/testutils.ts
@@ -1,11 +1,19 @@
 import * as _ from 'lodash'
-import Router, { NextRouter } from 'next/router'
+import { NextRouter } from 'next/router'
 
-export const mkRouter = ({ query }: { query: NextRouter['query'] }) => {
-  const router = _.clone(Router)
-  router.query = query
-  router.push = jest.fn()
-  return router
+type MockRouterConfig = {
+  query?: NextRouter['query']
+}
+
+export const mkRouter = (routerConfig: MockRouterConfig = {}): NextRouter => {
+  const { query = {} } = routerConfig
+
+  const mockRouter = {
+    query,
+    push: jest.fn(),
+  }
+
+  return (mockRouter as unknown) as NextRouter
 }
 
 export const getNewQuery = (router: NextRouter): Record<string, unknown> => {
