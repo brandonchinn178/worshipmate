e7e61e6f799b2240d091c85cfeee6e2587566f19
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-25 17:16:12 -0800

    Implement onConflict option

--- pg-toolbox/src/connection/insert.ts
+++ pg-toolbox/src/connection/insert.ts
@@ -1,4 +1,4 @@
-import { sql } from '../sql'
+import { sql, SqlQuery } from '../sql'
 
 export type InsertOptions = {
   onConflict?: ConflictOptions | null
@@ -9,6 +9,8 @@ export const mkInsertQueries = <T extends Record<string, unknown>>(
   records: T[],
   options: InsertOptions = {},
 ) => {
+  const { onConflict = null } = options
+
   return records.map((record) => {
     const columnNames = Object.keys(record)
     const values = columnNames.map((columnName) => record[columnName])
@@ -16,9 +18,16 @@ export const mkInsertQueries = <T extends Record<string, unknown>>(
     const columnNamesSql = sql.join(columnNames.map(sql.quote), ',')
     const valuesSql = sql.join(values.map(sql.param), ',')
 
+    const conflictClause = mkConflictClause(
+      columnNamesSql,
+      valuesSql,
+      onConflict,
+    )
+
     return sql`
       INSERT INTO ${sql.quote(table)} (${columnNamesSql})
       VALUES (${valuesSql})
+      ${conflictClause}
     `
   })
 }
@@ -29,3 +38,33 @@ type ConflictOptions =
   | 'ignore'
   | ({ action: 'ignore' } & Partial<ConflictTarget>)
   | ({ action: 'update' } & ConflictTarget)
+
+const mkConflictClause = (
+  columnNamesSql: SqlQuery,
+  valuesSql: SqlQuery,
+  options: ConflictOptions | null,
+) => {
+  if (options === null) {
+    return sql``
+  }
+
+  const { action, ...target } =
+    options === 'ignore' ? { action: 'ignore' } : options
+
+  const targetClause =
+    'constraint' in target && target.constraint
+      ? sql`ON CONSTRAINT ${sql.quote(target.constraint)}`
+      : 'column' in target && target.column
+      ? sql`(${sql.quote(target.column)})`
+      : sql``
+
+  switch (action) {
+    case 'ignore':
+      return sql`ON CONFLICT ${targetClause} DO NOTHING`
+    case 'update':
+      return sql`
+        ON CONFLICT ${targetClause} DO UPDATE
+        SET (${columnNamesSql}) = (${valuesSql})
+      `
+  }
+}
