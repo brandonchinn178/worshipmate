d231d188470a8f1e96a601fe8259900b455cbf20
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-26 13:59:06 -0800

    Add snapshot tests for expect failures

--- pg-toolbox/src/sql/testutils.spec.ts
+++ pg-toolbox/src/sql/testutils.spec.ts
@@ -169,3 +169,79 @@ describe('Match against query text and values', () => {
     ])
   })
 })
+
+describe('Failed match against query text', () => {
+  const query = sql`SELECT * FROM "song"`
+
+  test('expect().toMatchSql(string)', () => {
+    expect(() =>
+      expect(query).toMatchSql('SELECT * FROM "person"'),
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  test('expect().not.toMatchSql(string)', () => {
+    expect(() =>
+      expect(query).not.toMatchSql('SELECT * FROM "song"'),
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  test('expect.sqlMatching(string)', () => {
+    expect(() =>
+      expect([query]).toEqual([expect.sqlMatching('SELECT * FROM "person"')]),
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  test('expect.not.sqlMatching(string)', () => {
+    expect(() =>
+      expect([query]).toEqual([expect.not.sqlMatching('SELECT * FROM "song"')]),
+    ).toThrowErrorMatchingSnapshot()
+  })
+})
+
+describe('Failed match against query text and values', () => {
+  const name = 'Take On Me'
+  const query = sql`
+    SELECT * FROM "song"
+    WHERE "song"."name" = ${name}
+  `
+
+  test('expect().toMatchSql({ text, values })', () => {
+    expect(() =>
+      expect(query).toMatchSql({
+        text: 'SELECT * FROM "person" WHERE "person"."name" = $1',
+        values: [name],
+      }),
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  test('expect().not.toMatchSql({ text, values })', () => {
+    expect(() =>
+      expect(query).not.toMatchSql({
+        text: 'SELECT * FROM "song" WHERE "song"."name" = $1',
+        values: [name],
+      }),
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  test('expect.sqlMatching({ text, values })', () => {
+    expect(() =>
+      expect([query]).toEqual([
+        expect.sqlMatching({
+          text: 'SELECT * FROM "person" WHERE "person"."name" = $1',
+          values: [name],
+        }),
+      ]),
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  test('expect.not.sqlMatching({ text, values })', () => {
+    expect(() =>
+      expect([query]).toEqual([
+        expect.not.sqlMatching({
+          text: 'SELECT * FROM "song" WHERE "song"."name" = $1',
+          values: [name],
+        }),
+      ]),
+    ).toThrowErrorMatchingSnapshot()
+  })
+})
