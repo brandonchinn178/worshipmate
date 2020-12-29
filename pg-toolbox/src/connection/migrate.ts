8c86315da0cdf3122b59dee87c9dd9146badc979
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-28 17:26:41 -0800

    Rewrite loadFromArgs as standalone function

--- pg-toolbox/src/connection/migrate.ts
+++ pg-toolbox/src/connection/migrate.ts
@@ -1,71 +1,79 @@
-import { RunnerOption as NodePgMigrateOptions } from 'node-pg-migrate'
+import { RunnerOptionConfig as NodePgMigrateOptions } from 'node-pg-migrate/dist/types'
 
-export type MigrateOptions = Partial<NodePgMigrateOptions> & {
-  /**
-   * If true, load arguments from process.argv as well. Useful for migration
-   * scripts. Supports the following commands:
-   *
-   *   - up
-   *   - up {N}
-   *   - down
-   *   - down {N}
-   *   - redo
-   *   - redo {N}
-   */
-  loadFromArgs?: boolean
+export type MigrateAction = 'up' | 'down' | 'redo'
+
+export type MigrateOptions = Partial<
+  Omit<NodePgMigrateOptions, 'direction'> & {
+    action?: 'up' | 'down' | 'redo'
+  }
+>
+
+const getUsage = (scriptName: string) => `
+Usage: ${scriptName} [up|down|redo] [COUNT]
+
+Migrate the database with the given arguments.
+
+Parameters:
+  up|down|redo      The direction to migrate, with redo doing down first,
+                    then up.
+
+  COUNT             The number of migrations to run
+`
+
+const parseMigrateAction = (arg: string | undefined): MigrateAction | null => {
+  switch (arg) {
+    case 'up':
+      return 'up'
+    case 'down':
+      return 'down'
+    case 'redo':
+      return 'redo'
+    default:
+      return null
+  }
 }
 
-export type MigrateDirection = 'up' | 'down'
-
-export type MigrateDirectionOptions = Array<{
-  direction: MigrateDirection
-  count: number
-}>
-
-const parseMigrateCount = (arg: string | undefined) => {
+const parseMigrateCount = (arg: string | undefined): number | null => {
   if (arg === undefined) {
     return Infinity
   }
 
   const count = parseInt(arg, 10)
   if (isNaN(count)) {
-    throw new Error(`Invalid number: ${count}`)
+    return null
   }
 
   return count
 }
 
-const MIGRATE_DIRECTIONS: Record<string, MigrateDirection[]> = {
-  up: ['up'],
-  down: ['down'],
-  redo: ['down', 'up'],
-}
-
 /**
- * Should be called with process.argv.
+ * Load `MigrateOptions` from command line arguments.
  */
-export const parseMigrateArgs = (argv: string[]): MigrateDirectionOptions => {
+export const loadCLIMigrateArgs = (
+  argv: string[] = process.argv,
+): MigrateOptions => {
   // [node executable, script path, ...args]
   const [scriptName, ...args] = argv.slice(1)
 
-  const directions = MIGRATE_DIRECTIONS[args[0]]
-  if (directions === undefined) {
-    const usage = `
-      Usage: ${scriptName} [up|down|redo] [COUNT]
+  const action = parseMigrateAction(args[0])
+  if (action === null) {
+    const usage = getUsage(scriptName)
+    console.log(usage)
 
-      Migrate the database with the given arguments.
-
-      Parameters:
-        up|down|redo      The direction to migrate, with redo doing down first,
-                          then up.
-
-        COUNT             The number of migrations to run
-    `
-    console.log(usage.trim().replace(/^[ ]{6}/gm, ''))
-    process.exit(args[0] === '--help' || args[0] === 'help' ? 0 : 1)
+    switch (args[0]) {
+      case '--help':
+      case 'help':
+        process.exit(0)
+        break
+      default:
+        process.exit(1)
+    }
   }
 
   const count = parseMigrateCount(args[1])
+  if (count === null) {
+    throw new Error(`Invalid number: ${count}`)
+  }
 
-  return directions.map((direction) => ({ direction, count }))
+  return { action, count }
 }
