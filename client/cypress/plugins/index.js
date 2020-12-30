95f6b670d2e8e86d81e1054fdbbd8f2a63264f01
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-30 12:27:41 -0800

    Add @testing-library/cypress

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,5 +1,7 @@
+const wp = require('cypress-webpack-preprocessor-v5')
 const { spawnSync } = require('child_process')
 const path = require('path')
+const PnpWebpackPlugin = require('pnp-webpack-plugin')
 
 const TOP = path.resolve(process.cwd(), '..')
 
@@ -15,6 +17,29 @@ const yarn = (...args) => {
 
 module.exports = (on) => {
   process.env.NODE_ENV = 'test'
+
+  const options = {
+    webpackOptions: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: [/node_modules/],
+            use: 'babel-loader',
+          },
+        ],
+      },
+      resolve: {
+        plugins: [PnpWebpackPlugin],
+      },
+      resolveLoader: {
+        plugins: [PnpWebpackPlugin.moduleLoader(module)],
+      },
+    },
+    watchOptions: {},
+  }
+  on('file:preprocessor', wp(options))
 
   on('task', {
     clearDatabase() {
