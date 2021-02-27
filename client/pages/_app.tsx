8a52c7a98955d66d4735f61ef7c2a3a779a0f2bc
Brandon Chinn <brandon@leapyear.io>
2021-02-27 14:38:44 -0800

    Use our own ApolloProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,19 +1,16 @@
-import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import styled, { ThemeProvider } from 'styled-components'
 
-import { useApollo } from '~/apollo'
+import { ApolloProvider } from '~/apollo'
 import { Header } from '~/layout/Header'
 import { theme } from '~/theme'
 import { GlobalStyle } from '~/theme/global'
 
 export default function App(props: AppProps) {
-  const apolloClient = useApollo()
-
   return (
     <ThemeProvider theme={theme}>
-      <ApolloProvider client={apolloClient}>
+      <ApolloProvider>
         <AppContent {...props} />
       </ApolloProvider>
     </ThemeProvider>
