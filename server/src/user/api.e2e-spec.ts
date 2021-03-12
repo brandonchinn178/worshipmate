b4f69bd1b0429aa20389138b004ec4eeee60824a
Brandon Chinn <brandonchinn178@gmail.com>
2021-03-11 19:16:26 -0800

    Backfill tests for UserAPI

---
+++ server/src/user/api.e2e-spec.ts
@@ -0,0 +1,47 @@
+import * as fc from 'fast-check'
+import { sql } from 'pg-fusion'
+
+import { setupTestDatabase } from '~test-utils/db'
+
+import { UserAPI } from './api'
+
+const db = setupTestDatabase()
+const userApi = new UserAPI(db)
+
+const fcName = fc.string({ minLength: 1 })
+
+describe('UserAPI', () => {
+  describe('getOrCreate', () => {
+    it('can create a new user', async () => {
+      await fc.assert(
+        fc.asyncProperty(fcName, async (name) => {
+          await db.clear()
+          const user = await userApi.getOrCreate(name)
+          expect(user).toMatchObject({ name })
+        }),
+      )
+    })
+
+    it('can get an existing user', async () => {
+      await fc.assert(
+        fc.asyncProperty(fcName, async (name) => {
+          await db.clear()
+          await db.insertAll('user', [{ name }])
+
+          const { count: before } = await db.queryOne(sql`
+            SELECT COUNT(*) FROM "user"
+          `)
+
+          // should not error
+          const user = await userApi.getOrCreate(name)
+          expect(user).toMatchObject({ name })
+
+          const { count: after } = await db.queryOne(sql`
+            SELECT COUNT(*) FROM "user"
+          `)
+          expect(after).toEqual(before)
+        }),
+      )
+    })
+  })
+})
