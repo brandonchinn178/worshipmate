25b5293070a7d174fe9e4e44a9c41a5741b5b4cd
Brandon Chinn <brandon@leapyear.io>
2021-03-11 17:56:11 -0800

    Rewrite tests using screen

--- client/lib/ui-kit/DataTable.test.tsx
+++ client/lib/ui-kit/DataTable.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react'
+
 import { renderUI } from '~jest-utils'
 
 import { DataTable } from './DataTable'
@@ -16,42 +18,40 @@ const mockData: MockPerson[] = [
 it('displays data', () => {
   const columnDefs = [{ name: 'id' }, { name: 'name' }]
 
-  const { container, getByText } = renderUI(
+  const { container } = renderUI(
     <DataTable data={mockData} columnDefs={columnDefs} />,
   )
 
   expect(container).toMatchSnapshot()
-  expect(getByText('Id')).toBeVisible()
-  expect(getByText('Name')).toBeVisible()
-  expect(getByText('1')).toBeVisible()
-  expect(getByText('Alice')).toBeVisible()
-  expect(getByText('2')).toBeVisible()
-  expect(getByText('Bob')).toBeVisible()
-  expect(getByText('3')).toBeVisible()
-  expect(getByText('Charlie')).toBeVisible()
+  expect(screen.getByText('Id')).toBeVisible()
+  expect(screen.getByText('Name')).toBeVisible()
+  expect(screen.getByText('1')).toBeVisible()
+  expect(screen.getByText('Alice')).toBeVisible()
+  expect(screen.getByText('2')).toBeVisible()
+  expect(screen.getByText('Bob')).toBeVisible()
+  expect(screen.getByText('3')).toBeVisible()
+  expect(screen.getByText('Charlie')).toBeVisible()
 })
 
 it('displays no data', () => {
   const columnDefs = [{ name: 'id' }, { name: 'name' }]
 
-  const { container, getByText } = renderUI(
+  const { container } = renderUI(
     <DataTable data={[]} columnDefs={columnDefs} />,
   )
 
   expect(container).toMatchSnapshot()
-  expect(getByText('Id')).toBeVisible()
-  expect(getByText('Name')).toBeVisible()
+  expect(screen.getByText('Id')).toBeVisible()
+  expect(screen.getByText('Name')).toBeVisible()
 })
 
 it('displays a different header', () => {
   const columnDefs = [{ name: 'name', header: 'First Name' }]
 
-  const { getByText, queryByText } = renderUI(
-    <DataTable data={mockData} columnDefs={columnDefs} />,
-  )
+  renderUI(<DataTable data={mockData} columnDefs={columnDefs} />)
 
-  expect(getByText('First Name')).toBeVisible()
-  expect(queryByText('Name')).toBeNull()
+  expect(screen.getByText('First Name')).toBeVisible()
+  expect(screen.queryByText('Name')).toBeNull()
 })
 
 it('displays column with a custom render function', () => {
@@ -62,9 +62,7 @@ it('displays column with a custom render function', () => {
     },
   ]
 
-  const { getByText } = renderUI(
-    <DataTable data={mockData} columnDefs={columnDefs} />,
-  )
+  renderUI(<DataTable data={mockData} columnDefs={columnDefs} />)
 
-  expect(getByText('1+Alice')).toBeVisible()
+  expect(screen.getByText('1+Alice')).toBeVisible()
 })
