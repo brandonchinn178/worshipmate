c3eb53fd4dd7d2549625a01fb5492802e85887fe
Brandon Chinn <brandon@leapyear.io>
2021-04-08 21:27:58 -0700

    Add more login form tests

--- client/lib/auth/LoginForm.test.tsx
+++ client/lib/auth/LoginForm.test.tsx
@@ -5,6 +5,26 @@ import { renderUI, waitForAllStateChanges } from '~jest-utils'
 
 import { LoginForm } from './LoginForm'
 
+/** Mock react-toastify **/
+
+const mockToastError = jest.fn()
+jest.mock('react-toastify', () => {
+  return {
+    toast: {
+      error: (...args: unknown[]) => mockToastError(...args),
+    },
+  }
+})
+
+/** Mock console.error **/
+
+afterEach(jest.restoreAllMocks)
+
+const mockConsoleError = () =>
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+
+/** Tests **/
+
 it('renders the login form', () => {
   const { container } = renderUI(<LoginForm onSubmit={jest.fn()} />)
 
@@ -12,6 +32,27 @@ it('renders the login form', () => {
   expect(screen.getByLabelText('Username')).toBeVisible()
   expect(screen.getByLabelText('Password')).toBeVisible()
   expect(screen.getByText('Login', { selector: 'button' })).toBeVisible()
+})
+
+it('shows errors when onSubmit fails', async () => {
+  const e = new Error('onSubmit failed')
+  const onSubmit = jest.fn().mockRejectedValue(e)
+  const consoleError = mockConsoleError()
+
+  renderUI(<LoginForm onSubmit={onSubmit} />)
+
+  userEvent.type(screen.getByLabelText('Username'), 'user')
+  userEvent.type(screen.getByLabelText('Password'), 'pw')
+  userEvent.click(screen.getByText('Login'))
+
+  await waitFor(() => {
+    expect(onSubmit).toHaveBeenCalled()
+  })
+
+  expect(consoleError).toHaveBeenCalledWith(e)
+  expect(mockToastError).toHaveBeenCalledWith(
+    expect.stringContaining('onSubmit failed'),
+  )
 })
 
 describe('form validation', () => {
@@ -59,5 +100,61 @@ describe('form validation', () => {
 
     await waitForAllStateChanges()
     expect(onSubmit).not.toHaveBeenCalled()
+  })
+})
+
+describe('submit button', () => {
+  it('is enabled at start', async () => {
+    renderUI(<LoginForm onSubmit={jest.fn()} />)
+
+    expect(screen.getByRole('button')).not.toHaveAttribute('disabled')
+  })
+
+  it('is disabled during and after submitting', async () => {
+    const onSubmit = jest.fn()
+    renderUI(<LoginForm onSubmit={onSubmit} />)
+
+    userEvent.type(screen.getByLabelText('Username'), 'user')
+    userEvent.type(screen.getByLabelText('Password'), 'pw')
+    userEvent.click(screen.getByText('Login'))
+
+    expect(screen.getByRole('button')).toHaveAttribute('disabled')
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled()
+    })
+
+    expect(screen.getByRole('button')).toHaveAttribute('disabled')
+  })
+
+  it('is enabled after form validation fails', async () => {
+    const onSubmit = jest.fn()
+    renderUI(<LoginForm onSubmit={onSubmit} />)
+
+    userEvent.click(screen.getByText('Login'))
+
+    await waitForAllStateChanges()
+    expect(onSubmit).not.toHaveBeenCalled()
+
+    expect(screen.getByRole('button')).not.toHaveAttribute('disabled')
+  })
+
+  it('is enabled after submission fails', async () => {
+    const onSubmit = jest.fn().mockRejectedValue(new Error())
+    mockConsoleError()
+
+    renderUI(<LoginForm onSubmit={onSubmit} />)
+
+    userEvent.type(screen.getByLabelText('Username'), 'user')
+    userEvent.type(screen.getByLabelText('Password'), 'pw')
+    userEvent.click(screen.getByText('Login'))
+
+    expect(screen.getByRole('button')).toHaveAttribute('disabled')
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled()
+    })
+
+    expect(screen.getByRole('button')).not.toHaveAttribute('disabled')
   })
 })
