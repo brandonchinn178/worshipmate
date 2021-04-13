ed8d1de8f1471604a3ff52a939d7e2ab6fbf6950
Brandon Chinn <brandonchinn178@gmail.com>
2021-04-12 19:06:44 -0700

    Show typecheck errors in 'yarn start'

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,15 @@
 /* eslint-disable */
 
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 const path = require('path')
 
 module.exports = {
-  webpack(config, _) {
+  webpack(config, { dev, isServer }) {
     config.resolve.alias['~'] = path.join(__dirname, 'lib')
+
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
 
     return config
   },
