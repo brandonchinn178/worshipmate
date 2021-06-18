a635ddd903d614c085c29b94e50facb177b2fac5
Brandon Chinn <brandonchinn178@gmail.com>
2021-06-18 00:05:30 -0700

    Disable eslint in nextjs builds

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,9 @@ const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 const path = require('path')
 
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack(config, { dev, isServer }) {
     if (dev && isServer) {
       config.plugins.push(new ForkTsCheckerWebpackPlugin())
