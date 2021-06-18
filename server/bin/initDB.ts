274c7585043a35f626e9d5f9f882770e7061de0a
Brandon Chinn <brandonchinn178@gmail.com>
2021-06-18 16:38:48 -0700

    Add artist table

--- server/bin/initDB.ts
+++ server/bin/initDB.ts
@@ -3,14 +3,54 @@
 import { Database } from 'pg-fusion'
 
 import { withDatabase } from '~/db'
+import { ArtistRecord, SongRecord } from '~/song/schema'
 
 export const prepopulateDB = async (db: Database) => {
-  await db.insertAll(
+  const [
+    artistAllSonsAndDaughters,
+    artistBethel,
+    artistHousefires,
+    artistMattRedman,
+    artistMaverickCity,
+  ] = await db.insertAll<ArtistRecord>(
+    'artist',
+    [
+      {
+        slug: 'all-sons-and-daughters',
+        name: 'All Songs and Daughters',
+      },
+      {
+        slug: 'bethel-music',
+        name: 'Bethel Music',
+      },
+      {
+        slug: 'housefires',
+        name: 'Housefires',
+      },
+      {
+        slug: 'matt-redman',
+        name: 'Matt Redman',
+      },
+      {
+        slug: 'maverick-city-music',
+        name: 'Maverick City Music',
+      },
+    ],
+    {
+      onConflict: {
+        action: 'update',
+        column: 'slug',
+      },
+    },
+  )
+
+  await db.insertAll<SongRecord>(
     'song',
     [
       {
         slug: 'blessed-be-your-name',
         title: 'Blessed Be Your Name',
+        artist: artistMattRedman.id,
         recommended_key: 'A',
         time_signature_top: 4,
         time_signature_bottom: 4,
@@ -19,6 +59,7 @@ export const prepopulateDB = async (db: Database) => {
       {
         slug: 'build-my-life',
         title: 'Build My Life',
+        artist: artistHousefires.id,
         recommended_key: 'E',
         time_signature_top: 4,
         time_signature_bottom: 4,
@@ -27,18 +68,38 @@ export const prepopulateDB = async (db: Database) => {
       {
         slug: 'ever-be',
         title: 'Ever Be',
+        artist: artistBethel.id,
         recommended_key: 'E',
         time_signature_top: 4,
         time_signature_bottom: 4,
         bpm: 72,
       },
       {
+        slug: 'jireh',
+        title: 'Jireh',
+        artist: artistMaverickCity.id,
+        recommended_key: 'A',
+        time_signature_top: 4,
+        time_signature_bottom: 4,
+        bpm: 76,
+      },
+      {
         slug: 'great-are-you-lord',
         title: 'Great Are You Lord',
+        artist: artistAllSonsAndDaughters.id,
         recommended_key: 'G',
         time_signature_top: 6,
         time_signature_bottom: 8,
         bpm: 52,
+      },
+      {
+        slug: 'man-of-your-word',
+        title: 'Man of Your Word',
+        artist: artistMaverickCity.id,
+        recommended_key: 'G',
+        time_signature_top: 4,
+        time_signature_bottom: 4,
+        bpm: 72,
       },
     ],
     {
