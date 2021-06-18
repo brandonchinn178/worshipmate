8f7b8c6fafe7cb66863d935ffc7eb4a28ea91ea5
Brandon Chinn <brandonchinn178@gmail.com>
2021-06-17 19:45:43 -0700

    Show edit link in song page

--- client/pages/song/[slug]/index.tsx
+++ client/pages/song/[slug]/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
+import NextLink from 'next/link'
 
+import { useCurrentUserQuery } from '~/api/currentUser.generated'
 import { GetSongDocument } from '~/api/getSong.generated'
 import { getApolloClient } from '~/apollo'
 import { Song } from '~/song/models'
@@ -8,9 +10,23 @@ type SongPageProps = {
   song: Song
 }
 
+/* TODO: fill in */
 function SongPage({ song }: SongPageProps) {
-  // TODO
-  return JSON.stringify(song)
+  const { data: currentUserData } = useCurrentUserQuery()
+  const user = currentUserData?.me
+
+  return (
+    <div>
+      <p>{JSON.stringify(song)}</p>
+      {user && (
+        <p>
+          <NextLink href={`/song/${song.slug}/edit`}>
+            <a>Edit</a>
+          </NextLink>
+        </p>
+      )}
+    </div>
+  )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -27,7 +43,9 @@ export const getStaticProps: GetStaticProps<SongPageProps> = async ({
 
   const apolloClient = getApolloClient()
 
-  const { data: song } = await apolloClient.query({
+  const {
+    data: { song },
+  } = await apolloClient.query({
     query: GetSongDocument,
     variables: { slug },
   })
