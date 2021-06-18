a73b0584958966cc4786a3ef1a51e40838a0488f
Brandon Chinn <brandonchinn178@gmail.com>
2021-06-18 08:36:35 -0700

    Add next eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'plugin:jest/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier', 'jest'],
