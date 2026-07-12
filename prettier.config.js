165b4e28a5f49ffee5fb7b669d6fef79ec9b17f3
Brandon Chinn <brandonchinn178@gmail.com>
2026-07-12 14:41:00 -0700

    Add sveltekit template

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+	useTabs: false,
+	singleQuote: false,
+	trailingComma: 'all',
+	semi: false,
+	printWidth: 100,
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
+
+export default config;
