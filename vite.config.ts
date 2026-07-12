aacc72992273972ab683dde5c60216fb06e370b1
Brandon Chinn <brandonchinn178@gmail.com>
2026-07-12 14:52:16 -0700

    Use static site generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config"
 import { playwright } from "@vitest/browser-playwright"
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-static"
 import { sveltekit } from "@sveltejs/kit/vite"
 
 export default defineConfig({
