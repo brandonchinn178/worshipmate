0c25159fe082a25205e21aa680443d4bb16dfe2d
Brandon Chinn <brandonchinn178@gmail.com>
2026-07-28 19:32:27 -0700

    Fix env var build

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
+import { PUBLIC_SUPABASE_PUBLISHABLE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public"
+
 import type { Database } from "./types"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
-export const supabase = createClient<Database>(supabaseUrl, supabasePublishableKey)
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY)
