aa46ccc02bd044d542b38417771e130a32f2ba72
Brandon Chinn <brandonchinn178@gmail.com>
2026-09-12 17:06:38 -0700

    Generate keys data

---
+++ src/lib/songsheet/key.data.ts
@@ -0,0 +1,50 @@
+/* Generated with `npm run regen-keys-data` */
+
+export const KEYS = [
+  "C",
+  "Db",
+  "D",
+  "Eb",
+  "E",
+  "F",
+  "Gb",
+  "G",
+  "Ab",
+  "A",
+  "Bb",
+  "B"
+] as const
+export const BASE_KEYS = [
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "A",
+  "B"
+] as const
+export const KEY_DEGREES = {
+  "C": 0,
+  "Db": 1,
+  "D": 2,
+  "Eb": 3,
+  "E": 4,
+  "F": 5,
+  "Gb": 6,
+  "G": 7,
+  "Ab": 8,
+  "A": 9,
+  "Bb": 10,
+  "B": 11
+} as const
+export const KEY_ALIASES = {
+  "B#": "C",
+  "C#": "Db",
+  "D#": "Eb",
+  "Fb": "E",
+  "E#": "F",
+  "F#": "Gb",
+  "G#": "Ab",
+  "A#": "Bb",
+  "Cb": "B"
+} as const
--- src/lib/songsheet/key.ts
+++ src/lib/songsheet/key.ts
@@ -1,60 +1,17 @@
-export const KEYS = [
-  // keep-multiline
-  "C",
-  "Db",
-  "D",
-  "Eb",
-  "E",
-  "F",
-  "Gb",
-  "G",
-  "Ab",
-  "A",
-  "Bb",
-  "B",
-] as const
+import { BASE_KEYS, KEY_ALIASES, KEY_DEGREES, KEYS } from "./key.data.ts"
 
 export type Key = (typeof KEYS)[number]
 
-// A key is a base key if its length == 1
-type BaseKey<S extends string> = S extends `${infer _First}${infer Rest}`
-  ? Rest extends ""
-    ? S
-    : never
-  : never
+export { BASE_KEYS, KEYS }
 
-const isBaseKey = <K extends Key>(s: K): s is BaseKey<K> => s.length === 1
-export const BASE_KEYS = KEYS.filter(isBaseKey)
+const isAlias = (s: string): s is keyof typeof KEY_ALIASES => s in KEY_ALIASES
 
-const ENHARMONICS = {
-  "C#": "Db",
-  "D#": "Eb",
-  "E#": "F",
-  "F#": "Gb",
-  "G#": "Ab",
-  "A#": "Bb",
-  "B#": "C",
-  Cb: "B",
-  Db: "C#",
-  Eb: "D#",
-  Fb: "E",
-  Gb: "F#",
-  Ab: "G#",
-  Bb: "A#",
-} as const
-
-const endsWithSharp = <T extends string>(s: T): s is T & `${string}#` => {
-  return s.endsWith("#")
-}
-
-export const resolveKey = (key: Key | keyof typeof ENHARMONICS): Key => {
-  return endsWithSharp(key) || key === "Cb" || key == "Fb" // keep-multiline
-    ? ENHARMONICS[key]
-    : key
+export const resolveKey = (key: Key | keyof typeof KEY_ALIASES): Key => {
+  return isAlias(key) ? KEY_ALIASES[key] : key
 }
 
 export const transposeKey = (key: Key, n: number): Key => {
-  const oldKey = KEYS.indexOf(key)
+  const oldKey = KEY_DEGREES[key]
 
   let newKey = (oldKey + n) % KEYS.length
   if (newKey < 0) {
